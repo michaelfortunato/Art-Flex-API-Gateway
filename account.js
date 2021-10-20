20902296f5cae282fbc5b24746b67a8f68664c54
Michael Fortunato <michael.n.fortunato@gmail.com>
2021-10-19 22:42:09 -0400

    chunked upload

--- account.js
+++ account.js
@@ -10,6 +10,7 @@ const {
   UploadPartCommand,
   CompleteMultipartUploadCommand,
 } = require("@aws-sdk/client-s3");
+const { route } = require("./signup");
 const ACCOUNT_HOST =
   process.env.ACCOUNT_APP_SERVICE_SERVICE_HOST || "localhost";
 const ACCOUNT_PORT = process.env.ACCOUNT_APP_SERVICE_SERVICE_PORT || 8082;
@@ -30,125 +31,105 @@ router.get("/", async (req, res) => {
   }
 });
 
-let upload_table = {};
+let uploadTable = {};
 
-const initUpload = async (req, res) => {
-  const upload_id = req.headers["upload-id"];
-  const totalChunks = parseInt(req.headers["total-chunks"]);
-  const currentChunkNumber = parseInt(req.headers["current-chunk-number"]);
+const createUpload = async (req, res) => {
+  try {
+    const uploadId = req.headers["x-upload-id"];
+    const totalChunks = parseInt(req.headers["x-total-chunks"]);
 
-  if (upload_id in upload_table) {
-    req.status(500).send({statusMessage:"AWS multiupload currently in progress."})
-  }
+    if (uploadId in uploadTable) {
+      res
+        .status(500)
+        .send({ statusMessage: "AWS multiupload currently in progress." });
+    }
 
-  upload_table[upload_id] = {
+    uploadTable[uploadId] = {
       chunksProcessed: 0,
       totalChunks,
-      chunks: {}
+      chunks: {},
+      awsUploadId: null,
     };
- 
 
-  const createMultipartUpload = new CreateMultipartUploadCommand({
-    Bucket: "private.assets.art-flex.co",
-    ContentType: "application/octet-stream",
-    Key: upload_id,
-  });
-
-  const { UploadId: awsUploadId } = await req.app.locals.s3_client.send(
-    createMultipartUpload
-  );
-
+    const createMultipartUpload = new CreateMultipartUploadCommand({
+      Bucket: "private.assets.art-flex.co",
+      ContentType: "application/octet-stream",
+      Key: uploadId,
+    });
+    const { UploadId: awsUploadId } = await req.app.locals.s3_client.send(
+      createMultipartUpload
+    );
+    uploadTable[uploadId].awsUploadId = awsUploadId;
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 };
-const uploadPart = async (req, res, next) => {};
-const completeUpload = async (req, res, next) => {};
+const uploadPart = async (req, res, next) => {
+  try {
+    const uploadId = req.headers["x-upload-id"];
+    const totalChunks = parseInt(req.headers["x-total-chunks"]);
+    const currentChunkNumber = parseInt(req.headers["x-current-chunk-number"]);
+
+    if (
+      !("awsUploadId" in uploadTable[uploadId]) ||
+      uploadTable[uploadId].awsUploadId === null
+    ) {
+      res.sendStatus(500);
+    }
+    const uploadPartCommand = new UploadPartCommand({
+      Bucket: "private.assets.art-flex.co",
+      UploadId: uploadTable[uploadId].awsUploadId,
+      Key: uploadId,
+      PartNumber: currentChunkNumber,
+      Body: req.body,
+    });
+    const { ETag } = await req.app.locals.s3_client.send(uploadPartCommand);
+
+    uploadTable[uploadId].chunksProcessed += 1;
+    uploadTable[uploadId].chunks[currentChunkNumber] = ETag;
+
+    if (uploadTable[uploadId].chunksProcessed === totalChunks) {
+      next();
+    } else {
+      res.sendStatus(206);
+    }
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+};
+const completeUpload = async (req, res) => {
+  try {
+    const uploadId = req.headers["x-upload-id"];
+    const completeMultipartUpload = new CompleteMultipartUploadCommand({
+      Bucket: "private.assets.art-flex.co",
+      Key: uploadId,
+      UploadId: uploadTable[uploadId].awsUploadId,
+      MultipartUpload: {
+        Parts: Object.keys(uploadTable[uploadId].chunks).map((key) => ({
+          PartNumber: parseInt(key),
+          ETag: uploadTable[uploadId].chunks[key],
+        })),
+      },
+    });
+    const completeResponse = await req.app.locals.s3_client.send(
+      completeMultipartUpload
+    );
+    res.sendStatus(200);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+};
+
+router.post("/createUpload", createUpload);
 
 router.post(
-  "/create_post",
+  "/uploadPart",
   express.raw({ limit: "20mb" }),
-  async (req, res) => {
-    try {
-      //console.log(upload_table)
-      const upload_id = req.headers["upload-id"];
-      const totalChunks = parseInt(req.headers["total-chunks"]);
-      const currentChunkNumber = parseInt(req.headers["current-chunk-number"]);
-      if (!(upload_id in upload_table)) {
-        upload_table[upload_id] = {
-          chunksProcessed: 0,
-          totalChunks,
-          chunks: {},
-          awsUploadId: null,
-        };
-        // Initiate S3 multipart upload
-        const createMultipartUpload = new CreateMultipartUploadCommand({
-          Bucket: "private.assets.art-flex.co",
-          ContentType: "application/octet-stream",
-          Key: upload_id,
-        });
-        try {
-          const { UploadId: awsUploadId } = await req.app.locals.s3_client.send(
-            createMultipartUpload
-          );
-          upload_table[upload_id].awsUploadId = awsUploadId;
-        } catch (error) {
-          console.log("error");
-        }
-      }
-      const uploadPartCommand = new UploadPartCommand({
-        Bucket: "private.assets.art-flex.co",
-        UploadId: upload_table[upload_id].awsUploadId,
-        Key: upload_id,
-        PartNumber: currentChunkNumber,
-        Body: req.body,
-      });
-      const { ETag } = await req.app.locals.s3_client.send(uploadPartCommand);
-      upload_table[upload_id].chunksProcessed += 1;
-      upload_table[upload_id].chunks[currentChunkNumber] = ETag;
-
-      // Check to see if we can complete the upload
-      if (upload_table[upload_id].chunksProcessed === totalChunks) {
-        console.log(
-          Object.keys(upload_table[upload_id].chunks).map((key) => ({
-            PartNumber: parseInt(key),
-            ETag: upload_table[upload_id].chunks[key],
-          }))
-        );
-
-        const completeMultipartUpload = new CompleteMultipartUploadCommand({
-          Bucket: "private.assets.art-flex.co",
-          Key: upload_id,
-          UploadId: upload_table[upload_id].awsUploadId,
-          MultipartUpload: {
-            Parts: Object.keys(upload_table[upload_id].chunks).map((key) => ({
-              PartNumber: parseInt(key),
-              ETag: upload_table[upload_id].chunks[key],
-            })),
-          },
-        });
-
-        const completeResponse = await req.app.locals.s3_client.send(
-          completeMultipartUpload
-        );
-        res.sendStatus(200);
-      } else {
-        res.sendStatus(206);
-      }
-    } catch (error) {
-      //console.log(error);
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        res.status(error.response.status).send(error.response.data);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        res.status(500).send({ statusMessage: "Internal server error" });
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        res.status(500).send({ statusMessage: "Internal server error" });
-      }
-    }
-  }
+  uploadPart,
+  completeUpload
 );
 
 router.post("/studio/sell_item", async (req, res) => {});
